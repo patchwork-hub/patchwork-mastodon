61ae4beb8b66a695046ccde21344c86f7190d03d
Min Khant Kyaw <minkhantkyawmdy35@gmail.com>
2026-07-07 12:46:38 +0630

    Rollback Mastodon codes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ class RedirectWithVary < ActionDispatch::Routing::PathRedirect
 end
 
 def redirect_with_vary(path)
-  RedirectWithVary.new(301, path, caller_locations(1..1).first)
+  RedirectWithVary.new(301, path)
 end
 
 Rails.application.routes.draw do
